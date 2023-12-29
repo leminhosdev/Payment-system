487207e63bfebc9772f3c060c6cfb54539db79fe
leminhosdev <lucaslemos8294@gmail.com>
2023-12-29 14:54:12 -0300

    Banco de dados configurado/ docker compose configurado

---
+++ src/main/java/com/leminhosdev/paymentsystem/PaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leminhosdev.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leminhosdev/paymentsystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leminhosdev.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
